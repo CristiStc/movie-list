a4def4766ceca2163ad170678b1d8e22a984576a
ChrisBNC <cristianpaulstoica@gmail.com>
2019-11-18 20:40:23 +0200

    movie search and display results

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,46 +1,106 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import { Row, Col, Input, Button } from "antd";
 import axios from "axios";
 
 import settings from "../../config";
+
+const IMAGE_PATH = "https://image.tmdb.org/t/p/w200/";
+const ResultList = props => {
+  const { results } = props;
+  return results.map(item => {
+    return (
+      <Row key={item.id} gutter={8} className="result_item">
+        <Col span={4} className="result_poster">
+          <img
+            src={IMAGE_PATH + item.poster_path}
+            alt={`Poster of ${item.title}`}
+          />
+        </Col>
+        <Col span={12}>
+          <span>{item.title} </span>
+        </Col>
+        <Col span={4}>
+          <span>{item.vote_average} </span>
+        </Col>
+        <Col span={4}>
+          <span>{item.release_date} </span>
+        </Col>
+      </Row>
+    );
+  });
+};
 export class Home extends Component {
   state = {
-    searchTerm: ""
+    searchTerm: "",
+    results: [],
+    isLoading: false
   };
 
   handleSearchChange = event => {
     const { value } = event.target;
+
+    if (value.length === 0) {
+      this.setState({ results: [] });
+    }
+
     this.setState({
       searchTerm: value
     });
   };
 
-  handleSearchClick = () => {
+  handleSearchClick = async () => {
     console.log(this.state.searchTerm);
 
     const url = `
     https://api.themoviedb.org/3/search/movie?api_key=${settings.APIKEY}&query=${this.state.searchTerm}`;
 
+    // Async/Await example
+    // let response = null;
+    // try {
+    //   response = await axios.get(url);
+    // } catch (e) {
+    //   console.log(e);
+    // }
+    // console.log(res.data);
+
+    this.setState({ isLoading: true });
     axios.get(url).then(res => {
+      this.setState({ results: res.data.results, isLoading: false });
       console.log(res.data);
     });
   };
   render = () => {
+    const { searchTerm, isLoading } = this.state;
     return (
-      <Row>
-        <Col span={8} offset={6}>
-          <Input
-            placeholder="Search for a movie"
-            value={this.state.searchTerm}
-            onChange={this.handleSearchChange}
-          />
-        </Col>
-        <Col span={2}>
-          <Button type="primary" icon="search" onClick={this.handleSearchClick}>
-            Search
-          </Button>
-        </Col>
-      </Row>
+      <Fragment>
+        <Row>
+          <Col span={8} offset={6}>
+            <Input
+              placeholder="Search for a movie"
+              value={searchTerm}
+              onChange={this.handleSearchChange}
+              onPressEnter={this.handleSearchChange}
+              allowClear
+            />
+          </Col>
+          <Col span={2}>
+            <Button
+              loading={isLoading}
+              disabled={searchTerm.length < 2}
+              type="primary"
+              icon="search"
+              onClick={this.handleSearchClick}
+            >
+              Search
+            </Button>
+          </Col>
+        </Row>
+        <Row>
+          <Col span={10} offset={6}>
+            <ResultList results={this.state.results} />
+          </Col>
+        </Row>
+      </Fragment>
     );
   };
 }
